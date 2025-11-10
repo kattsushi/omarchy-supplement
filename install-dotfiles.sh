2628fb81c8912046ddacea44439d5bd71cbe79c0
Andres Jimenez <andres@outlook.com>
2025-11-09 21:19:39 -0600

    adding nvim for default configs

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -19,18 +19,19 @@ cd ~
 if [ -d "$REPO_NAME" ]; then
   echo "Repository '$REPO_NAME' already exists. Skipping clone"
 else
-  git clone "$REPO_URL" "$REPO_NAME"
+  git clone "$REPO_URL" "$REPO"
 fi
 
 # Check if the clone was successful
 if [ $? -eq 0 ]; then
   echo "removing old configs"
-  rm -rf ~/.config/nvim ~/.config/starship.toml ~/.config/ghostty/config
+  rm -rf ~/.config/nvim ~/.config/starship.toml ~/.local/share/nvim/ ~/.cache/nvim/ ~/.config/ghostty/config
 
   cd "$REPO_NAME"
   stow zshrc
   stow ghostty
   stow tmux
+  stow nvim
   stow starship
 else
   echo "Failed to clone the repository."
